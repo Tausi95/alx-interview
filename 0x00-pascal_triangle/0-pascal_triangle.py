56e3587ed8e771497a774e0304974ddad24e559c
Mage107CT <effortbuildingcontractors@gmail.com>
2024-10-05 17:58:52 +0200

    fixed header

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,16 +1,17 @@
+#!/usr/bin/python3
+"""Module for pascal_triangle function"""
+
+
 def pascal_triangle(n):
+    """Returns a list of lists of integers representing
+    the Pascal’s triangle of n"""
     if n <= 0:
         return []
-
-    triangle = [[1]]  # Start with the first row
-
+    triangle = [[1]]
     for i in range(1, n):
-        row = [1]  # Every row starts with a 1
-        prev_row = triangle[i - 1]
-        # Generate the middle values of the row
+        row = [1]
         for j in range(1, i):
-            row.append(prev_row[j - 1] + prev_row[j])
-        row.append(1)  # Every row ends with a 1
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
         triangle.append(row)
-
     return triangle
