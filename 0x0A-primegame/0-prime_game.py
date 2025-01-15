5955899b69469cb2cf3409abcc8cbdf3ebc08191
Mage107CT <effortbuildingcontractors@gmail.com>
2025-01-15 14:57:02 +0200

    change code functionality

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,46 +1,23 @@
 #!/usr/bin/python3
+"""0. Prime Game
+"""
 
-def is_prime(n):
-    """Helper function to check if a number is prime."""
-    if n <= 1:
-        return False
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
-
-def play_game(n):
-    """Simulate a single round of the prime game."""
-    nums = list(range(1, n + 1))
-    primes = [p for p in nums if is_prime(p)]
-    
-    turn = 0  # 0 for Maria, 1 for Ben
-    while primes:
-        # Pick the smallest prime and remove all its multiples
-        prime = primes[0]
-        nums = [x for x in nums if x % prime != 0]
-        primes = [p for p in nums if is_prime(p)]
-        
-        # Switch turns
-        turn = 1 - turn
-    
-    # If turn is 0, Maria wins (because it was Ben's turn and couldn't play)
-    return 'Maria' if turn == 1 else 'Ben'
 
 def isWinner(x, nums):
-    """Determine who won the most rounds."""
-    maria_wins = 0
-    ben_wins = 0
-    
-    for n in nums:
-        winner = play_game(n)
-        if winner == 'Maria':
-            maria_wins += 1
-        elif winner == 'Ben':
-            ben_wins += 1
-    
-    if maria_wins > ben_wins:
+    """determines the winner of the game"""
+    participants = {'Maria': 0, 'Ben': 0}
+    if x == 0 or x is None:
+        return
+    for i in range(x):
+        vals = [num for num in range(1, nums[i] + 1)]
+        currentTurn = 'Maria'
+        while vals != []:
+            currentPick = vals.pop(0)
+            for num in vals:
+                if num % currentPick == 0:
+                    vals.remove(num)
+            currentTurn = 'Ben' if currentTurn == 'Maria' else 'Maria'
+        participants[currentTurn] += 1
+    if participants['Maria'] > participants['Ben']:
         return 'Maria'
-    elif ben_wins > maria_wins:
-        return 'Ben'
-    return None
+    return 'Ben'
